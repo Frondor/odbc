7b5a0713fd1012dbea108e5c8ed37955963e62d0
Federico <Frondor@users.noreply.github.com>
2017-02-25 16:38:45 -0300

    Added support for L5.4 Connection::resolveFor()

--- src/TCK/Odbc/OdbcServiceProvider.php
+++ src/TCK/Odbc/OdbcServiceProvider.php
@@ -40,15 +40,31 @@ class OdbcServiceProvider extends ServiceProvider {
 	 */
 	public function boot()
 	{
-		Connection::resolverFor('odbc', function ($connection, $database, $prefix, $config) {
-		    if ( ! isset( $config['prefix'] ) )
-		    {
-			$config['prefix'] = '';
-		    }
-		    $connector = new ODBCConnector();
-		    $pdo       = $connector->connect( $config );
-		    return new ODBCConnection($pdo, $database, $config['prefix'], $config);
-		});
+
+		if (version_compare($this->app::VERSION, '5.4', '>')) {
+			Connection::resolverFor('odbc', function ($connection, $database, $prefix, $config) {
+			    if ( ! isset( $config['prefix'] ) )
+			    {
+				$config['prefix'] = '';
+			    }
+			    $connector = new ODBCConnector();
+			    $pdo       = $connector->connect( $config );
+			    return new ODBCConnection($pdo, $database, $config['prefix'], $config);
+			});
+		}
+		else { // TO BE REMOVED
+			$factory = $this->app['db'];
+			$factory->extend( 'odbc', function ( $config )
+			{
+				if ( ! isset( $config['prefix'] ) )
+				{
+					$config['prefix'] = '';
+				}
+				$connector = new ODBCConnector();
+				$pdo       = $connector->connect( $config );
+				return new ODBCConnection( $pdo, $config['database'], $config['prefix'], $config );
+			} );
+		}
 	}
 
 }
